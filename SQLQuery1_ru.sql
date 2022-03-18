00ee36e51c7f7e013416f2e9bb394d66d067b5c7
Ekaterina Maleeva <61910699+EkaterinaMaleeva@users.noreply.github.com>
2022-03-18 14:18:19 +0700

    Add files via upload

---
+++ SQLQuery1_ru.sql
@@ -0,0 +1,94 @@
+-- Выведем данные, которые планируем использовать
+
+Select location, date, total_cases, new_cases, total_deaths, population
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Order by 1,2
+
+
+-- Посмотрим на Total Cases vs Total Deaths
+-- Получим вероятность смерти, если вы находитесь в США
+Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where location like '%states%' and continent is not null
+Order by 1,2
+
+
+-- Посмотрим на Total Cases vs Population
+-- Найдем процент населения, который заболел Covid
+Select location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Where location like '%states%' and continent is not null
+Order by 1,2
+
+
+-- Выведем страны с самым высоким показателем заболеваемости (Highest Infection Rate) в сравнении с населением (Population)
+Select location, population, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population)*100) as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by location, population
+Order by PercentPopulationInfected desc
+
+-- Выведем страны с самым болишим количеством смертей
+Select location, MAX(total_deaths) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by location
+Order by TotalDeathCount desc
+
+-- Выведем количество смертей по континентам
+Select continent, MAX(total_deaths) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Group by continent
+Order by TotalDeathCount desc
+
+
+-- Мировые показатели
+Select SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+Where continent is not null
+Order by 1,2
+
+
+-- Посмотрим на Total Population vs Vaccinations
+
+With PopvsVac (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+	, SUM(vac.new_vaccinations) OVER (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+Where dea.continent is not null
+)
+Select *, (RollingPeopleVaccinated/population)*100
+From PopvsVac
+
+--TEMP TABLE
+
+DROP table if exists #PercentPopulationVaccinated
+Create table #PercentPopulationVaccinated
+(
+continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+	, SUM(vac.new_vaccinations) OVER (Partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
+	--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+--Where dea.continent is not null
+
+Select *, (RollingPeopleVaccinated/population)*100
+From #PercentPopulationVaccinated
+
